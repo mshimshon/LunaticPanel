a1e81603e33a41ea4b1d35a9fec6e2cee207b66e
Maksim Shimshon <m.samson@opencodedev.com>
2026-06-12 15:10:36 +0300

    - Added Service Registration for Medihater on PackageManager

--- src/Plugins/PackageManager/LunaticPanel.PackageManager.Application/RegisterServicesExt.cs
+++ src/Plugins/PackageManager/LunaticPanel.PackageManager.Application/RegisterServicesExt.cs
@@ -1,18 +1,27 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Wolverine;
+﻿using LunaticPanel.PackageManager.Application.Mediator.Commands.Handlers;
+using LunaticPanel.PackageManager.Application.Mediator.Queries.Handlers;
+using MedihatR;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace LunaticPanel.PackageManager.Application;
 
 public static class RegisterServicesExt
 {
+    public static bool _medihaterScanned = false;
     public static void AddApplicationServices(this IServiceCollection services)
     {
-        services.ConfigureWolverine(options =>
-        {
-            // Explicitly force Wolverine to scan this Application assembly for handlers
-            options.Discovery.IncludeAssembly(typeof(RegisterServicesExt).Assembly);
-
-            // Optional: Configure outbox, error handling policies, or brokers here
-        });
+        services.AddMedihaterServices();
+        services.AddMedihaterHandler<RepositorySourceRemoveHandler>();
+        services.AddMedihaterHandler<RepositorySourceEnableHandler>();
+        services.AddMedihaterHandler<RepositorySourceDisableHandler>();
+        services.AddMedihaterHandler<RepositorySourceAddHandler>();
+        services.AddMedihaterHandler<PackageUpdateHandler>();
+        services.AddMedihaterHandler<PackageRemoveHandler>();
+        services.AddMedihaterHandler<PackageInstallHandler>();
+        services.AddMedihaterHandler<PackageEnableHandler>();
+        services.AddMedihaterHandler<PackageDisableHandler>();
+        services.AddMedihaterHandler<SearchPackagesHandler>();
+        services.AddMedihaterHandler<SearchRepositoryHandler>();
     }
 }
